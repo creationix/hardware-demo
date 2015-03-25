1f0ff49ea881c05229454fcece502971c6b320bf
Tim Caswell <tim@creationix.com>
2015-03-24 22:49:39 -0500

    Make friendlier

--- demo.lua
+++ demo.lua
@@ -1,24 +1,52 @@
 local uv = require('uv')
 local pig = require('pig')
 
+local alive = true
+
+
+
+
 local function main()
 
   local req, watch, cleanup = pig()
 
   local inputs = {4,5,17,27}
   local outputs = {6, 13, 19, 26}
-  -- Set pins 4, 5, 17, and 27 as input (buttons)
-  req("m " .. table.concat(inputs, " r m ") .. " r")
-  -- Enable internal pull-down resistors on buttons
-  req("pud " .. table.concat(inputs, " d pud ") .. " d")
-  -- Set mode for 6, 13, 19, and 26 for output (LEDs)
-  req("m " .. table.concat(outputs, " w m ") .. " w")
-  -- Turn all LEDs off
-  req("w " .. table.concat(outputs, " 0 w ") .. " 0") 
   
+  print "Setting pins 4, 5, 17, and 27 as input"
+  req("m " .. table.concat(inputs, " r m ") .. " r")
+  
+  print "Enabling internal pull-down resistors on inputs"
+  req("pud " .. table.concat(inputs, " d pud ") .. " d")
+  
+  print "Setting pins 6, 13, 19, and 26 as output"
+  req("m " .. table.concat(outputs, " w m ") .. " w")
+  
+  print "Reseting outputs to low"
+  req("w " .. table.concat(outputs, " 0 w ") .. " 0") 
+ 
+  uv.new_signal():start("sigint", function ()
+    coroutine.wrap(function ()
+      print "Reseting outputs to low"
+      req("w " .. table.concat(outputs, " 0 w ") .. " 0") 
+      print "Cleaning pig library resources"
+      cleanup()
+      print "Closing all libuv handles"
+      uv.walk(function (handle)
+        if not handle:is_closing() then
+          handle:close()
+        end
+      end)
+      uv.stop()
+    end)()
+    
+  end)
+
+  print "Waiting for changes to inputs and writing state to outputs"
+  print "Press Control+C to exit"
   local old = 0
   local read = watch(unpack(inputs))
-  for n = 1, 100 do
+  while true do
     local event = read()
     for i = 1, #inputs do
       local pin = inputs[i]
@@ -31,7 +59,7 @@ local function main()
     end
     old = event.level
   end
-  cleanup()
+
 end
 
 assert(xpcall(function ()
