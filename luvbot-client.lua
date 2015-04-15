056d4bef18450d76ca248765bd75ed6d5c3c5fed
Tim Caswell <tim@creationix.com>
2015-04-15 12:17:40 -0500

    Move luvbot to use gamepad library so it works on osx and windows too

--- luvbot-client.lua
+++ luvbot-client.lua
@@ -1,14 +1,24 @@
+-- Tweak this to the host of ip of your robot
+local luvbot = "192.168.1.105"
+
+local jit = require('jit')
+jit.off()
+local G = require('gamepad')
+
 local tcp = require('coro-tcp')
 local uv = require('uv')
-local makeJoy = require('joy')
-local bit = require('bit')
 
 local ox, oy, x, y, read, write
+x = 0
+y = 0
+
+G.Gamepad_init()
 
 local function tick()
+  G.Gamepad_processEvents()
   if x == ox and y == oy then return end
   ox, oy = x, y
-  local left, right = -y / 128 - x / 128, -y / 128 + x / 128
+  local left, right = y / 128 - x / 128, y / 128 + x / 128
   -- left, right = 0, 0
   local command
   if left < -20 then
@@ -28,22 +38,21 @@ local function tick()
   write(command)
 
 end
+
+local function onAxis(_, axis, value)
+  if axis == 2 then -- x axis
+    x = -value * 256 * 128
+  elseif axis == 1 then -- y axis
+    y = value * 256 * 128
+  end
+end
+
 local function main()
-  read, write = assert(tcp.connect("cherry", 1337))
+  read, write = assert(tcp.connect(luvbot, 1337))
   p(read, write)
   write("m 27 w m 22 w m 23 2 m 24 w p 27 0 p 22 0 p 23 0 p 24 0")
-  local get, close = makeJoy(0)
+  G.Gamepad_axisMoveFunc(onAxis, nil)
   print("Connected to remote")
-  for struct in get do
-    if bit.band(struct.type, 0x02) > 0 then -- axis event
-      if struct.number == 3 then -- x axis
-        x = struct.value
-      elseif struct.number == 1 then -- y axi
-        y = struct.value
-      end
-    end
-  end
-  close()
 end
 
 assert(xpcall(function ()
---
+++ package.lua
@@ -0,0 +1,9 @@
+return {
+  name = "creationix/luvbot",
+  version = "0.0.0",
+  private = true,
+  dependencies = {
+    "creationix/gamepad",
+    "creationix/coro-tcp",
+  }
+}
